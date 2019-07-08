c1acecee2b4ce93cd0488de1b751b084fb50aaa8
shubhsy <32479973+shubhsy@users.noreply.github.com>
2019-07-08 13:19:45 +0530

    Create models.py

---
+++ microblog/app/models.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    firstname = db.Column(db.String(64), index=True, unique=True)
+    lastname = db.Column(db.String(64), index=True, unique=True)
+    phoneno = db.Column(db.String(10), index=True, unique=True)
+    employeecode = db.Column(db.String(10), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    email = db.Column(db.String(64), index=True, unique=True)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
