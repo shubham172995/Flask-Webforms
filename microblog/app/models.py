f4f937ded0a66c12d472c4520b3828e067848669
shubhsy <32479973+shubhsy@users.noreply.github.com>
2019-07-22 18:00:08 +0530

    Update models.py

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -1,33 +1,48 @@
 from datetime import datetime
 from app import db, login
-from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.security import generate_password_hash
 from flask_login import UserMixin
 
-class User(UserMixin, db.Model):
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
-    firstname = db.Column(db.String(64), index=True, unique=True)
-    lastname = db.Column(db.String(64), index=True, unique=True)
+    firstname = db.Column(db.String(64), index=True)
+    lastname = db.Column(db.String(64), index=True)
     phoneno = db.Column(db.String(10), index=True, unique=True)
     employeecode = db.Column(db.String(10), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     email = db.Column(db.String(64), index=True, unique=True)
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    otp=db.Column(db.String(4), index=True)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+        return self.password_hash==password
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
-class Post(db.Model):
+class VerifiedUser(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    username = db.Column(db.String(64), index=True, unique=True)
+    firstname = db.Column(db.String(64), index=True)
+    lastname = db.Column(db.String(64), index=True)
+    phoneno = db.Column(db.String(10), index=True, unique=True)
+    employeecode = db.Column(db.String(10), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    email = db.Column(db.String(64), index=True, unique=True)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return self.password_hash==password
 
     def __repr__(self):
-        return '<Post {}>'.format(self.body)
+        return '<User {}>'.format(self.username)
+
